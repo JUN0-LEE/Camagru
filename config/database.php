b628da3479f82dab215d50a35ec00318ae668946
Juno <48269210+joonito@users.noreply.github.com>
2021-03-02 11:37:56 +0900

    Update database.php

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 define('DB_SERVER', '127.0.0.1');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'tha3849Leejh!');
+define('DB_PASSWORD', 'root');
 define('DB_NAME', 'demo0');
 // echo "Hello1";
 ?>
